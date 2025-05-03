3352b706491c20d0b08b1aee7e8ee08b4bfdc607
Aravind <Aravind@DESKTOP-OVC0NH1.mshome.net>
2025-05-03 08:01:38 +0530

    new project

---
+++ BProject/src/test/java/Testcases/Testcase_001.java
@@ -0,0 +1,14 @@
+package Testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Testcase_001 {
+    @Test
+    public void openGoogle() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.quit();
+    }
+}
